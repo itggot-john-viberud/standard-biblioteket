ec665a947a768b6f0b0c721e68ed623db4194808
itggot-john-viberud <john.viberud@itggot.se>
2018-04-18 13:09:24 +0200

    even and odd

---
+++ lib/even.rb
@@ -0,0 +1,6 @@
+def even(term1)
+    if term1 % 2 == 0
+        return true
+    end
+    return false
+end
---
+++ lib/odd.rb
@@ -0,0 +1,6 @@
+def odd(term1)
+    if term1 % 2 == 0
+        return false
+    end
+    return true
+end
