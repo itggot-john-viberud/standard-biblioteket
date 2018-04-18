82c52f6ebb72f2326c83118f0a9f2f4bf95e4af1
itggot-john-viberud <john.viberud@itggot.se>
2018-04-18 13:32:16 +0200

    glömde att ha lika med i between

--- lib/between.rb
+++ lib/between.rb
@@ -1,7 +1,7 @@
 def between(term1, term2, term3)
-    if term1 > term2 && term1 < term3
+    if term1 >= term2 && term1 <= term3
         return true
-    elsif term1 < term2 && term1 > term3
+    elsif term1 <= term2 && term1 >= term3
         return true
     end
     return false
