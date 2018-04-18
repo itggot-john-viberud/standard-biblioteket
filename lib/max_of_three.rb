cac98cdc9e49b622f67422227b774842ca6a057a
itggot-john-viberud <john.viberud@itggot.se>
2018-04-18 13:39:01 +0200

    störst av 3

---
+++ lib/max_of_three.rb
@@ -0,0 +1,9 @@
+def max_of_three(term1, term2 ,term3)
+    if term1 > term2 && term1 > term3
+        return term1
+    elsif term2 > term1 && term2 > term3
+        return term2
+    else
+        return term3
+    end
+end
